009cd00a6bae4feb5117168abf94486fda396f44
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-09-24 12:24:27 -0400

    get spots route created

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
+
+const { Spot } = require('../../db/models');
+
+const router = express.Router();
+
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
+
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll();
+
+    // if(!spots){}
+
+    return res.json({Spots: spots})
+})
+
+
+
+
+
+module.exports = router;
