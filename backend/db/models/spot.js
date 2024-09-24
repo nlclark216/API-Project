db77d356b17ecd115a62ec53ecd02580dcda355b
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-09-24 12:12:12 -0400

    created spots api file

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -20,43 +20,73 @@ module.exports = (sequelize, DataTypes) => {
     },
     address: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+        len: [2, 256]
+      }
     },
     city: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [2, 45],
+        isAlpha: true
+      }
     },
     state: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [2, 2],
-        isUppercase: true
+        len: [2, 45],
+        isAlpha: true
       }
     },
     country: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [2, 256],
+        isAlpha: true
+      }
     },
     lat: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        len: [6, 15]
+      }
     },
     lng: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        len: [6, 15]
+      }
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 256],
+        isAlpha: true
+      }
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [3, 500]
+      }
     },
     price: {
       type: DataTypes.DECIMAL,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      }
     },
   }, {
     sequelize,
