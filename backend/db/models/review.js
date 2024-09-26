408fbfc9a6a108d91f258aa6c73c76b4275a4c36
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-09-25 20:31:14 -0400

    added userId associations

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Review.belongsTo(models.Spot, { foreignKey: "spotId" });
+      Review.belongsTo(models.User, { foreignKey: "userId" });
       Review.hasMany(models.ReviewImage, { foreignKey: "reviewId", onDelete: 'cascade', hooks: true });
     }
   }
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,7 +4,8 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      User.hasMany(models.Spot, { foreignKey: "ownerId", onDelete: 'cascade', hooks: true })
+      User.hasMany(models.Spot, { foreignKey: "ownerId", onDelete: 'cascade', hooks: true });
+      User.hasMany(models.Review, { foreignKey: "userId", onDelete: 'cascade', hooks: true })
     }
   }
   User.init({
