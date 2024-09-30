533aaa0428ce7e4e8be32d2846c29240c053eaf9
Nia C <157457101+nlclark216@users.noreply.github.com>
2024-09-30 18:34:38 -0400

    spot association error

--- backend/db/migrations/20240926005107-spot-association.js
+++ backend/db/migrations/20240926005107-spot-association.js
@@ -16,14 +16,8 @@ module.exports = {
       onDelete: 'cascade',
       allowNull: false
     });
-    await queryInterface.addColumn(options, 'avgRating', {
-      type: Sequelize.INTEGER,
-      references: { model: 'Reviews' },
-      onDelete: 'cascade'
-    });
   },
   async down (queryInterface, Sequelize) {
     await queryInterface.removeColumn(options, 'ownerId');
-    await queryInterface.removeColumn(options, 'avgRating');
   }
 };
